59d2d7230e54dc4794d208856ae41516f668c457
alican yılmaz <alicanyilmaz101@gmail.com>
2024-03-17 12:44:20 -0400

    SignUpViewModel Display attributes added.

--- AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -1,10 +1,22 @@
-﻿namespace AspNetCoreIdentityApp.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
 {
-    public class SignUpViewModel(string? userName, string? email, string? phone, string? password)
+    public class SignUpViewModel(string? userName, string? email, string? phone, string? password,string? passwordConfirm)
     {
+        [Display(Name = "User Name")]
         public string? UserName { get; set; } = userName;
+
+        [Display(Name = "Email")]
         public string? Email { get; set; } = email;
+
+        [Display(Name = "Phone")]
         public string? Phone { get; set; } = phone;
+
+        [Display(Name = "Password")]
         public string? Password { get; set; } = password;
+
+        [Display(Name = "Password Confirm")]
+        public string?  PasswordConfirm { get; set; } = passwordConfirm;
     }
 }
