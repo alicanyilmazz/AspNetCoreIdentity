e3999225cab394022a675783219badff0601d86c
alican yılmaz <alicanyilmaz101@gmail.com>
2024-03-16 22:53:05 -0400

    SignUpViewModel created.

---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel(string? userName, string? email, string? phone, string? password)
+    {
+        public string? UserName { get; set; } = userName;
+        public string? Email { get; set; } = email;
+        public string? Phone { get; set; } = phone;
+        public string? Password { get; set; } = password;
+    }
+}
