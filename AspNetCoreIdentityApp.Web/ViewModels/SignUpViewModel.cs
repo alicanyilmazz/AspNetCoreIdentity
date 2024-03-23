7d8b749115f8637e3dffdeeeb67a3e7d654a8043
alican yılmaz <alicanyilmaz101@gmail.com>
2024-03-23 10:18:59 -0400

    SignUpViewModel validations added.

--- AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -2,21 +2,43 @@
 
 namespace AspNetCoreIdentityApp.Web.ViewModels
 {
-    public class SignUpViewModel(string? userName, string? email, string? phone, string? password,string? passwordConfirm)
+    public class SignUpViewModel
     {
+        [Required(ErrorMessage ="User name can not be empty!")]
         [Display(Name = "User Name")]
-        public string? UserName { get; set; } = userName;
+        public string? UserName { get; set; }
 
+        [Required(ErrorMessage = "Email can not be empty!")]
+        [EmailAddress(ErrorMessage = "Invalid email address!")]
         [Display(Name = "Email")]
-        public string? Email { get; set; } = email;
+        public string? Email { get; set; }
 
+        [Required(ErrorMessage = "Phone can not be empty!")]
+        [RegularExpression(@"^\+(?:[0-9] ?){6,14}[0-9]$", ErrorMessage = "Invalid phone number!")]
         [Display(Name = "Phone")]
-        public string? Phone { get; set; } = phone;
+        public string? Phone { get; set; }
 
+        [Required(ErrorMessage = "Password can not be empty!")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long!")]
+        [MaxLength(16, ErrorMessage = "Password can not be longer than 16 characters!")]
         [Display(Name = "Password")]
-        public string? Password { get; set; } = password;
+        public string? Password { get; set; }
 
+        [Compare(nameof(Password), ErrorMessage = "Password and password confirm do not match!")]
+        [Required(ErrorMessage = "Password confirm can not be empty!")]
         [Display(Name = "Password Confirm")]
-        public string?  PasswordConfirm { get; set; } = passwordConfirm;
+        public string?  PasswordConfirm { get; set; }
+        public SignUpViewModel()
+        {
+                
+        }
+        public SignUpViewModel(string? userName, string? email, string? phone, string? password, string? passwordConfirm)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+            PasswordConfirm = passwordConfirm;
+        }
     }
 }
