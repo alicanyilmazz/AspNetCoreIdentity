d98ee0822d13d4747baf39850108e37d3fa15781
alican yılmaz <alicanyilmaz101@gmail.com>
2024-03-23 10:18:46 -0400

    SignUp Method created.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -23,8 +27,35 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult SignUp()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var identityResult = await _userManager.CreateAsync(
+            new AppUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.Phone,
+            }, request.Password);
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Registration was completed successfully.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+            foreach (IdentityError error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
             return View();
         }
 
