550e17413806c401fd27c89f2da5bb9697618306
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-06 15:24:05 -0400

    GetUsersAsync() created.

--- AspNetCoreIdentityApp.Core/Services/IMemberService.cs
+++ AspNetCoreIdentityApp.Core/Services/IMemberService.cs
@@ -1,8 +1,10 @@
 ﻿using AspNetCoreIdentityApp.Core.Entities;
+using AspNetCoreIdentityApp.Core.ViewModels.Areas.Admin;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +13,6 @@ namespace AspNetCoreIdentityApp.Core.Services
     public interface IMemberService
     {
         public Task<IdentityResult> CreateAsync(AppUser user,string password);
+        public Task<IEnumerable<UserViewModel>> GetUsersAsync();
     }
 }
