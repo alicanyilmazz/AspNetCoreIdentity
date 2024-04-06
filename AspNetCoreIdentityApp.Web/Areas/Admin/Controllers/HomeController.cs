392979fdad4e54747bfc27eb02bbaa4a8d327614
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-06 15:48:20 -0400

    ctor injection added.

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreIdentityApp.Core.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMemberService _memberService;
+
+        public HomeController(ILogger<HomeController> logger, IMemberService memberService)
+        {
+            _logger = logger;
+            _memberService = memberService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Users()
+        public async Task<IActionResult> Users()
         {
-            return View();
+            return View(await _memberService.GetUsersAsync());
         }
     }
 }
