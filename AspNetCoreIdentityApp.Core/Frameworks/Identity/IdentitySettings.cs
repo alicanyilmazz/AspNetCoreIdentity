a3425882326257b2cb298b9e9ca724552beda12f
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-10 22:19:38 -0400

    ConfigureIdentitySettings created

---
+++ AspNetCoreIdentityApp.Core/Frameworks/Identity/IdentitySettings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentityApp.Core.Frameworks.Identity
+{
+    public class IdentitySettings
+    {
+        public static Action<IdentityOptions> ConfigureIdentitySettings()
+        {
+            return options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.SignIn.RequireConfirmedEmail = true;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+                options.SignIn.RequireConfirmedAccount = true;
+            };
+        }
+    }
+}
