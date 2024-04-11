0a397e4a08b58229c5f332dc627df2053b82f4de
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-10 22:20:02 -0400

    UserValidator created.

---
+++ AspNetCoreIdentityApp.Core/Frameworks/Identity/UserValidator.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreIdentityApp.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityApp.Core.Frameworks.Identity
+{
+    public class UserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            var errors = new List<IdentityError>();
+            if (user.UserName!.Length > 0 && !char.IsLetter(user.UserName![0]))
+            {
+                errors.Add(new IdentityError() { Code = "UserNameFirstCharacterMustBeLetter", Description = "Username first character must be letter" });
+            }
+            if (user.UserName!.Contains("admin"))
+            {
+                errors.Add(new IdentityError() { Code = "UserNameContainAdmin", Description = "Username can not contain admin" });
+            }
+            if (user.Email!.EndsWith(".com"))
+            {
+                errors.Add(new IdentityError() { Code = "EmailEndsWithCom", Description = "Email can not end with .com" });
+            }
+            if (errors.Any())
+            {
+                return Task.FromResult(IdentityResult.Failed([.. errors]));
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
