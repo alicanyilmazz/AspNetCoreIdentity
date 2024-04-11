7cd130c790da7ab87d825cbad2243c647269ccde
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-10 22:42:09 -0400

    LocalizationIdentityErrorDescriber created for customization error messages.

---
+++ AspNetCoreIdentityApp.Core/Frameworks/Identity/Localization.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityApp.Core.Frameworks.Identity
+{
+    public class Localization
+    {
+        public string Language { get; set; }
+        public Localization() 
+        {
+            Language = "en";
+        }
+    }
+}
