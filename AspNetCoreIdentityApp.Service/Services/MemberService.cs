b0b283ad2b98b4356bc2038d7eb6a6cc908ab091
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-11 22:47:55 -0400

    SignInManager added

--- AspNetCoreIdentityApp.Service/Services/MemberService.cs
+++ AspNetCoreIdentityApp.Service/Services/MemberService.cs
@@ -15,17 +15,44 @@ namespace AspNetCoreIdentityApp.Service.Services
     public class MemberService : IMemberService
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public MemberService(UserManager<AppUser> userManager)
+        public MemberService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public async Task<IdentityResult> CreateAsync(AppUser user, string password)
+        public async Task<IdentityResult> SignUpAsync(AppUser user, string password)
         {
              return await _userManager.CreateAsync(user,password);
         }
 
+        public async Task<IdentityResult> SignInAsync(string email, string password,bool rememberMe)
+        {
+            var errors = new List<IdentityError>();
+            var hasUser = await _userManager.FindByEmailAsync(email);
+            if (hasUser is null)
+            {
+                errors.Add(new IdentityError() { Code = "EmailOrPasswordWrong", Description = "Email or password wrong" });
+                return IdentityResult.Failed([.. errors]);
+            }
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, password, rememberMe, true);
+            if (signInResult.Succeeded)
+            {
+                return IdentityResult.Success;
+            }
+
+            if (signInResult.IsLockedOut)
+            {
+                errors.Add(new IdentityError() { Code = "UserLockedOut", Description = "You can not sign in for 15 minutes." });
+                return IdentityResult.Failed([.. errors]);
+            }
+            var accessFailedCount = await _userManager.GetAccessFailedCountAsync(hasUser);
+            errors.Add(new IdentityError() { Code = "EmailOrPasswordWrong", Description = "Email or password wrong." });
+            errors.Add(new IdentityError() { Code = "EmailOrPasswordWrongCount", Description = $"Unsuccessful entrance {accessFailedCount}." });
+            return IdentityResult.Failed([.. errors]);
+        }
         public async Task<IEnumerable<UserViewModel>> GetUsersAsync()
         {
             return ObjectMapper.Mapper.Map<IEnumerable<UserViewModel>>(await _userManager.Users.ToListAsync());
