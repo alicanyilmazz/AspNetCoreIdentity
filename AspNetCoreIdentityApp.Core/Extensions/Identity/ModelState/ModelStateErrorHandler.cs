d80ed7551138c76e8767059d957718d260ba5547
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-11 22:46:33 -0400

    AddModelStateErrors extension created.

---
+++ AspNetCoreIdentityApp.Core/Extensions/Identity/ModelState/ModelStateErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityApp.Core.Extensions.Identity.ModelState
+{
+    public static class ModelStateErrorHandler
+    {
+        public static void AddModelStateErrors(this ModelStateDictionary modelState,List<string> errors)
+        {
+            errors.ForEach(error => 
+            {
+                modelState.AddModelError(string.Empty, error);
+            });
+        }
+    }
+}
