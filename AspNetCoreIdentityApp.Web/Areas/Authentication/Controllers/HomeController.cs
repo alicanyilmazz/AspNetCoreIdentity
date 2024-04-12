b73455664a41e81481b7d587dbaf5cf27bed83c9
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-11 22:48:23 -0400

    SignIn Get and Post methods created.

--- AspNetCoreIdentityApp.Web/Areas/Authentication/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Authentication/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentityApp.Core.Entities;
+using AspNetCoreIdentityApp.Core.Extensions.Identity.ModelState;
 using AspNetCoreIdentityApp.Core.Services;
 using AspNetCoreIdentityApp.Web.Areas.Authentication.ViewModels;
 using Microsoft.AspNetCore.Identity;
@@ -35,23 +36,48 @@ namespace AspNetCoreIdentityApp.Web.Areas.Authentication.Controllers
             {
                 return View();
             }
-            var identityResult = await _memberService.CreateAsync(
+            var identityResult = await _memberService.SignUpAsync(
             new AppUser
             {
                 UserName = request.UserName,
                 Email = request.Email,
                 PhoneNumber = request.Phone,
-            }, request.Password);
+            }, request.Password!);
             if (identityResult.Succeeded)
             {
                 TempData["SuccessMessage"] = "Registration was completed successfully.";
                 return RedirectToAction(nameof(HomeController.SignUp));
             }
-            foreach (IdentityError error in identityResult.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
+            ModelState.AddModelStateErrors(identityResult.Errors.Select(x=>x.Description).ToList());
+
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel request,string? returnUrl=null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            returnUrl = returnUrl ?? Url.Action("Index", "Home", new { area = "Admin" });
+
+            var response = await _memberService.SignInAsync(request.Email, request.Password, request.RememberMe);
+
+            if (!response.Succeeded) 
+            {
+                ModelState.AddModelStateErrors(response.Errors.Select(x => x.Description).ToList());
+                return View();
+            }
+
+            return Redirect(returnUrl);
         }
     }
 }
