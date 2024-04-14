5a3c20e2543d5815d6fcfe1972fbd8c3d90f1665
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-14 10:51:09 -0400

    New User Area created.

---
+++ AspNetCoreIdentityApp.Web/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
