92a76ec0c5a78f7e5fa6fed9256d8609e9842b64
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-19 20:33:08 -0400

    EmailService created.

---
+++ AspNetCoreIdentityApp.Service/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using AspNetCoreIdentityApp.Core.Services;
+using AspNetCoreIdentityApp.Core.ViewModels.Areas.Authentication;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityApp.Service.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _emailSettings = options.Value;
+        }
+
+        public async Task SendPasswordResetEmail(string link, string toEmail)
+        {
+            var smtpClient = new SmtpClient();
+            smtpClient.Host = _emailSettings.Host;
+            smtpClient.Port = Convert.ToInt16(_emailSettings.Port);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new System.Net.NetworkCredential(_emailSettings.Email, _emailSettings.Password);
+            
+            var mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(_emailSettings.Email);
+            mailMessage.To.Add(toEmail);
+            mailMessage.Subject = "Reset Password";
+            mailMessage.Body = @$"<h4>Please use link below for reset your password.</h4>
+                                <a href='{link}'>Reset Password</a>";
+            mailMessage.IsBodyHtml = true;
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
