b9a7670081bdb5562f64be744cdb3f736f6f0708
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-20 17:41:13 -0400

    DataType attribute added.

--- AspNetCoreIdentityApp.Web/Areas/Authentication/ViewModels/ResetPasswordViewModel.cs
+++ AspNetCoreIdentityApp.Web/Areas/Authentication/ViewModels/ResetPasswordViewModel.cs
@@ -4,12 +4,14 @@ namespace AspNetCoreIdentityApp.Web.Areas.Authentication.ViewModels
 {
     public class ResetPasswordViewModel
     {
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password can not be empty!")]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters long!")]
         [MaxLength(16, ErrorMessage = "Password can not be longer than 16 characters!")]
         [Display(Name = "New Password")]
         public string? Password { get; set; }
 
+        [DataType(DataType.Password)]
         [Compare(nameof(Password), ErrorMessage = "Password and password confirm do not match!")]
         [Required(ErrorMessage = "Password confirm can not be empty!")]
         [Display(Name = "New Password Confirm")]
