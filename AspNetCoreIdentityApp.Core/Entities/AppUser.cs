7a860e20edea1321ecb79fe9dcd5a33053b8e586
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-27 21:52:48 -0400

    City,Picture,BirthDate,Gender added to AppUser

--- AspNetCoreIdentityApp.Core/Entities/AppUser.cs
+++ AspNetCoreIdentityApp.Core/Entities/AppUser.cs
@@ -4,5 +4,9 @@ namespace AspNetCoreIdentityApp.Core.Entities
 {
     public class AppUser : IdentityUser
     {
+        public string? City { get; set; }
+        public string? Picture { get; set; }
+        public DateTime? BirtDate { get; set; }
+        public byte? Gender { get; set; }
     }
 }
