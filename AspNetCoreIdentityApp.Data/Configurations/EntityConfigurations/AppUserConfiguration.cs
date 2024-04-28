d4f24abc762d71e085751cb536bc0d259cf29a40
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-27 21:53:25 -0400

    AppUserConfiguration created for AspNetUsers table columns configurations.

---
+++ AspNetCoreIdentityApp.Data/Configurations/EntityConfigurations/AppUserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreIdentityApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityApp.Data.Configurations.EntityConfigurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(u => u.City).HasMaxLength(100).IsRequired(false);
+            builder.Property(u => u.Picture).IsRequired(false);
+            builder.Property(u => u.BirtDate).HasColumnType("date").IsRequired(false);
+            builder.Property(u => u.Gender).IsRequired(false);
+        }
+    }
+}
