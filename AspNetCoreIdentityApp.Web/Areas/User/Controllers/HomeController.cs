1dd7b8f3c9aaa2a2c54d51ac7bad596006a27995
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-27 21:28:37 -0400

    Profile and PasswordChange added.

--- AspNetCoreIdentityApp.Web/Areas/User/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Areas/User/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentityApp.Core.Entities;
+using AspNetCoreIdentityApp.Core.Extensions.Identity.ModelState;
 using AspNetCoreIdentityApp.Core.Services;
 using AspNetCoreIdentityApp.Web.Areas.User.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -23,12 +24,42 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
             (AppUser user, IdentityResult result) = await _memberService.GetUserByNameAsync(User.Identity!.Name!);
             if (!result.Succeeded)
             {
-                TempData["ErrorMessage"] = result.Errors.Select(x=>x.Description);
+                TempData["ErrorMessage"] = result.Errors.Select(x => x.Description);
             }
-            return View(new UserViewModel { Email = user.Email , PhoneNumber = user.PhoneNumber , UserName = user.UserName});
+            return View(new UserViewModel { Email = user.Email, PhoneNumber = user.PhoneNumber, UserName = user.UserName });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PasswordChange()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response = await _memberService.ChangeUserPasswordAsync(User.Identity!.Name!, request.CurrentPassword, request.NewPassword);
+            if (!response.Succeeded)
+            {
+                ModelState.AddModelStateErrors(response.Errors.Select(x => x.Description).ToList());
+                return View();
+            }
+            TempData["SuccessMessage"] = $"Your password changed successfully.";
+            return View();
         }
     }
 }
