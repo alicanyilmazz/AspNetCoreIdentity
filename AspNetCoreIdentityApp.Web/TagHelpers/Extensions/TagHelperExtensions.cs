9c8e56888376af2a230c4acf403ab5b9257af837
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-28 17:48:55 -0400

    TagHelper Extension created

---
+++ AspNetCoreIdentityApp.Web/TagHelpers/Extensions/TagHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCoreIdentityApp.Web.TagHelpers.Extensions
+{
+    public static class TagHelperExtensions
+    {
+        public static void SetBaseElementFeatures(this TagHelperOutput? output, string tagName, TagMode tagMode = TagMode.StartTagAndEndTag)
+        {
+            if (output is not null && !string.IsNullOrEmpty(tagName))
+            {
+                output.TagName = tagName;
+            }
+            if (output is not null && !string.IsNullOrEmpty(tagName))
+            {
+                output.TagMode = tagMode;
+            }
+        }
+        public static void SetAttributeIfNotNullOrEmpty(this TagHelperAttributeList? attributes, string attributeName, string? attributeValue)
+        {
+            if (attributes is not null && !string.IsNullOrEmpty(attributeValue))
+            {
+                attributes.SetAttribute(attributeName, attributeValue);
+            }
+        }
+    }
+}
