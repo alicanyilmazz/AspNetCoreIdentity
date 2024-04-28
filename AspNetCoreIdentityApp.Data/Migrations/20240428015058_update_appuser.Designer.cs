ed1d192cac9f85ff7e59fd09e728833e99d82933
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-27 22:01:05 -0400

    Migration created for new AspNetUsers table columns.

--- AspNetCoreIdentityApp.Data/Migrations/20240428015058_update_appuser.Designer.cs
+++ AspNetCoreIdentityApp.Data/Migrations/20240428015058_update_appuser.Designer.cs
@@ -4,6 +4,7 @@ using AspNetCoreIdentityApp.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AspNetCoreIdentityApp.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240428015058_update_appuser")]
+    partial class update_appuser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AspNetCoreIdentityApp.Data/Migrations/20240428015058_update_appuser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreIdentityApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_appuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirtDate",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "tinyint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Picture",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirtDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Picture",
+                table: "AspNetUsers");
+        }
+    }
+}
