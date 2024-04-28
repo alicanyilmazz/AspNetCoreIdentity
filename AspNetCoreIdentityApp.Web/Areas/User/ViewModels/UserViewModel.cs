4b0979f5605ccaade8837c3e25b2c75c24d2531d
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-28 17:46:59 -0400

    ProfileImageUrl added

--- AspNetCoreIdentityApp.Web/Areas/User/ViewModels/UserViewModel.cs
+++ AspNetCoreIdentityApp.Web/Areas/User/ViewModels/UserViewModel.cs
@@ -5,5 +5,6 @@
         public string? UserName { get; set; } 
         public string? Email { get; set; } 
         public string? PhoneNumber { get; set; }
+        public string? ProfileImageUrl { get; set; }
     }
 }
