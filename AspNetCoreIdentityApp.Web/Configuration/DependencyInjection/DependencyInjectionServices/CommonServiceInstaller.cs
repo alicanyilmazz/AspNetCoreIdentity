da203ba31530b5e95123cc04c8e35ec1c05f6aad
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-28 17:48:35 -0400

    CommonServiceInstaller created for common DI objects like an FileProvider.

---
+++ AspNetCoreIdentityApp.Web/Configuration/DependencyInjection/DependencyInjectionServices/CommonServiceInstaller.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreIdentityApp.Core.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace AspNetCoreIdentityApp.Web.Configuration.DependencyInjection.DependencyInjectionServices
+{
+    public class CommonServiceInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+        }
+    }
+}
