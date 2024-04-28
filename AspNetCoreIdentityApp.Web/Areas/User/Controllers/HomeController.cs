c26ceb788cb386121822c7fd707b0481fb9ca897
alican yılmaz <alicanyilmaz101@gmail.com>
2024-04-28 17:46:48 -0400

    UserInformationChange HttpPost Action developed

--- AspNetCoreIdentityApp.Web/Areas/User/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Areas/User/Controllers/HomeController.cs
@@ -2,11 +2,13 @@
 using AspNetCoreIdentityApp.Core.Entities.Enums;
 using AspNetCoreIdentityApp.Core.Extensions.Identity.ModelState;
 using AspNetCoreIdentityApp.Core.Services;
+using AspNetCoreIdentityApp.Core.ViewModels.Areas.User;
 using AspNetCoreIdentityApp.Web.Areas.User.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.FileProviders;
 
 namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
 {
@@ -16,11 +18,12 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
     {
         private readonly IMemberService _memberService;
         private readonly IEmailService _emailService;
-
-        public HomeController(IMemberService memberService, IEmailService emailService)
+        private readonly IFileProvider _fileProvider;
+        public HomeController(IMemberService memberService, IEmailService emailService, IFileProvider fileProvider)
         {
             _memberService = memberService;
             _emailService = emailService;
+            _fileProvider = fileProvider;
         }
 
         [HttpGet]
@@ -37,7 +40,8 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
             {
                 TempData["ErrorMessage"] = result.Errors.Select(x => x.Description);
             }
-            return View(new UserViewModel { Email = user.Email, PhoneNumber = user.PhoneNumber, UserName = user.UserName });
+
+            return View(new UserViewModel { Email = user.Email, PhoneNumber = user.PhoneNumber, UserName = user.UserName, ProfileImageUrl = user.Picture });
         }
 
         [HttpGet]
@@ -57,7 +61,7 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
             var response = await _memberService.ChangeUserPasswordAsync(User.Identity!.Name!, request.CurrentPassword, request.NewPassword);
             if (!response.Succeeded)
             {
-                ModelState.AddModelStateErrors(response.Errors.Select(x => x.Description).ToList());
+                ModelState.AddModelStateErrors(response.Errors);
                 return View();
             }
             TempData["SuccessMessage"] = $"Your password changed successfully.";
@@ -68,10 +72,10 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
         public async Task<IActionResult> UserInformationChange()
         {
             ViewBag.Genders = new SelectList(Enum.GetNames(typeof(Gender)));
-            (AppUser currentUser , IdentityResult result) = await _memberService.GetUserByNameAsync(User.Identity!.Name!);
+            (AppUser currentUser, IdentityResult result) = await _memberService.GetUserByNameAsync(User.Identity!.Name!);
             if (!result.Succeeded)
             {
-                ModelState.AddModelStateErrors(result.Errors.Select(x => x.Description).ToList());
+                ModelState.AddModelStateErrors(result.Errors);
                 return View();
             }
             var user = new UserInformationChangeViewModel
@@ -89,7 +93,49 @@ namespace AspNetCoreIdentityApp.Web.Areas.User.Controllers
         [HttpPost]
         public async Task<IActionResult> UserInformationChange(UserInformationChangeViewModel request)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var profileInformation = new UserProfileInformationViewModel
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                Phone = request.Phone,
+                City = request.City,
+                BirtDate = request.BirtDate,
+                Gender = request.Gender
+            };
+
+            if (request.ProfileImage is not null && request.ProfileImage.Length > 0)
+            {
+                var wwwrootFolder = _fileProvider.GetDirectoryContents("wwwroot");
+                var randomFileName = $"{Guid.NewGuid()}{Path.GetExtension(request.ProfileImage.FileName)}";
+                var newProfileImagePath = Path.Combine(wwwrootFolder.First(x => x.Name == "images").PhysicalPath, randomFileName);
+                using var stream = new FileStream(newProfileImagePath, FileMode.Create);
+                await request.ProfileImage.CopyToAsync(stream);
+                profileInformation.ProfileImage = randomFileName;
+            }
+            var result = await _memberService.UpdateUserInformationAsync(profileInformation, User.Identity!.Name!);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelStateErrors(result.Errors);
+                return View();
+            }
+
+            TempData["SuccessMessage"] = $"Your information updated successfully.";
+
+            var updatedUserInformation = new UserInformationChangeViewModel
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                Phone = request.Phone,
+                City = request.City,
+                BirtDate = request.BirtDate,
+                Gender = request.Gender
+            };
+            return View(updatedUserInformation);
         }
     }
 }
