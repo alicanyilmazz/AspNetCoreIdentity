72dcfd98b231f2352890094cd4317efb3926088a
alican yılmaz <alicanyilmaz101@gmail.com>
2024-05-12 13:57:28 -0400

    RoleUpdateViewModel created

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/ViewModels/RoleUpdateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.ViewModels
+{
+    public class RoleUpdateViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Role Name can not be empty!")]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+    }
+}
